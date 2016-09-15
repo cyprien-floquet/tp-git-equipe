55396ad5518a0e3363e9f642f96df230ff3ecb47
cyprien.floquet@lmdsio.fr <>
2016-09-15 17:18:28 +0200

    Modification titre pageManga.php

--- pageManga.php
+++ pageManga.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Mangas</title>
+        <title>Mangas de Cyprien</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet"> 
